a2b063995e675899b200e329cbf961b1f6f69921
Thrilok28021996 <thriloke96@gmail.com>
2025-09-30 11:02:58 +0530

    Update

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
              template_name='accounts/password_reset.html',
              email_template_name='accounts/password_reset_email.html',
              subject_template_name='accounts/password_reset_subject.txt',
-             success_url='/accounts/password-reset/done/'
+             success_url='/password-reset/done/'
          ),
          name='password_reset'),
     path('password-reset/done/',
@@ -33,7 +33,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(
              template_name='accounts/password_reset_confirm.html',
-             success_url='/accounts/password-reset-complete/'
+             success_url='/password-reset-complete/'
          ),
          name='password_reset_confirm'),
     path('password-reset-complete/',
